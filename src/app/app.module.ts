288e0f33f00a08edffb8ef15b41ffe137b48bd98
Aleksa <lukicaleksa04@gmail.com>
2020-11-13 13:20:46 +0100

    Create basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegistationComponent } from './registation/registation.component';
+import { OfficeOverviewComponent } from './office-overview/office-overview.component';
+import { CaseOverviewComponent } from './case-overview/case-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistationComponent,
+    OfficeOverviewComponent,
+    CaseOverviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/office-overview/office-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-office-overview',
+  templateUrl: './office-overview.component.html',
+  styleUrls: ['./office-overview.component.css']
+})
+export class OfficeOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
