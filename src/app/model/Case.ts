f4401e31a5ec0d7f79fbb8890fb34ceffcb4870c
Aleksa <lukicaleksa04@gmail.com>
2020-11-14 10:07:12 +0100

    Create models

---
+++ src/app/model/Case.ts
@@ -0,0 +1,7 @@
+
+export class Case {
+	
+	id:number;
+	creation_date:Date;
+	note:string;
+}
--- src/app/service/case.service.ts
+++ src/app/service/case.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Case } from '../model/Case';
+import { GenericService } from './generic.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CaseService {
-
-  constructor() { }
+export class CaseService extends GenericService<Case> {
+  route = "case";
 }
