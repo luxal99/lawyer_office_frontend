865c2580e403d62158454c6436cca92639d42703
Aleksa <lukicaleksa04@gmail.com>
2020-11-15 16:20:51 +0100

    Open cl-dialog from case

--- src/app/office-overview/case-overview/case-overview-dialog/case-overview-dialog.component.ts
+++ src/app/office-overview/case-overview/case-overview-dialog/case-overview-dialog.component.ts
@@ -1,7 +1,8 @@
 import { formatDate } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { Case } from 'src/app/model/Case';
+import { ClientOverviewDialogComponent } from '../../client-overview/client-overview-dialog/client-overview-dialog.component';
 
 @Component({
   selector: 'app-case-overview-dialog',
@@ -12,13 +13,25 @@ export class CaseOverviewDialogComponent implements OnInit {
 
   
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Case) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Case,private dialog:MatDialog) { }
 
   ngOnInit() {
     this.data.listOfLawsuits.forEach(lawsuit => {
       lawsuit.date_formatted = formatDate(lawsuit.date, 'dd/MM/yyyy', 'en-US');
     })
 
+  }
+
+  openClientOverview(client): void {
+    const dialogRef = this.dialog.open(ClientOverviewDialogComponent, {
+      minWidth: '70%',
+      position: { right: '0' },
+      height: '100vh',
+      data:client
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
   }
 
 }
